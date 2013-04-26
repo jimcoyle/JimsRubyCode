808e9e7e2cdc80aecfb844a23aec2ae49f9d18b9
Jim Coyle <jim.coyle@sage.com>
2013-04-26 11:37:43 +0100

    branch test

--- angry_boss.rb
+++ angry_boss.rb
@@ -1,6 +1,7 @@
 puts 'What do you want?'
 reply = gets.chomp
 puts 'Whaddaya mean ' '"' +reply.upcase+ '?!?"' ' You\'re fired!!'
+puts 'this is a branch'
 
 if reply == 'it\'s only me'
 puts 'ok - no worries'
